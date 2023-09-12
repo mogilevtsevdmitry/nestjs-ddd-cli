e3be4d3171b43761dfa3ae6ac9bd07fa28e609b9
Mogilevtsev Dmitry <webmogilevtsev@ya.ru>
2023-09-13 01:21:28 +0500

    UPD: refactoring

--- src/commands/events/files/name-event-handler.ts
+++ src/commands/events/files/name-event-handler.ts
@@ -1,15 +1,11 @@
 import { capitalizeName } from '@utils';
 
-export const EVENT_HANDLER = (name: string) => `import { Logger } from '@nestjs/common';
-import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+export const EVENT_HANDLER = (name: string) => `import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { ${capitalizeName(name)}Event } from './${name}.event';
 
 @EventsHandler(${capitalizeName(name)}Event)
 export class ${capitalizeName(name)}EventHandler implements IEventHandler {
-    private readonly logger = new Logger(${capitalizeName(name)}EventHandler.name);
-    constructor() {}
-
-    handle(event: ${capitalizeName(name)}Event): ${capitalizeName(name)}Event {
+    handle(event: ${capitalizeName(name)}Event) {
         // your code here
         return event;
     }
